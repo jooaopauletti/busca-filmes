c7c3a1a1d38f542e0315223b289da56d81cbf327
João Pauletti <jooaoalvez@gmail.com>
2026-07-15 08:52:08 -0300

    adiciona busca de filmes via API OMDb

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -9,7 +10,18 @@ API_KEY = os.getenv('OMDB_API_KEY')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    titulo = request.args.get('titulo')
+    filme = None
+
+    if titulo:
+        params = {
+            "apikey": API_KEY,
+            "t": titulo
+        }
+        resposta = requests.get("http://www.omdbapi.com/", params=params)
+        filme = resposta.json()
+
+    return render_template('index.html', filme=filme)
 
 if __name__ == '__main__':
     app.run(debug=True)
