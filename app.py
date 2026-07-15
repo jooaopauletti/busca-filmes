94559a879a6e3820ccd1c8719d20332167e50bbc
João Pauletti <jooaoalvez@gmail.com>
2026-07-15 08:32:13 -0300

    adiciona estrutura HTML e CSS da pagina de busca

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 
 load_dotenv()
 API_KEY = os.getenv('OMDB_API_KEY')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
